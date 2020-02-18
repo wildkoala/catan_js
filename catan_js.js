ed5568ee2b7445a81c9645e367f1d8315857fc82
Twiggy Gordon <31520629+wildkoala@users.noreply.github.com>
2020-02-18 14:15:06 -0500

    Adds a couple player methods
    Adds a hand with several cards in it, and has a show hand function to see what cards a player has.

--- catan_js.js
+++ catan_js.js
@@ -1,29 +1,32 @@
 class Player {
   constructor(name) {
     this.p_name = name;
-    this.p_hand = NULL; // Gonna be an array
-    this.num_cards = NULL;
-    this.p_color = NULL;
-    this.p_victory_pts = NULL;
-    this.p_dev_cards = NULL;
+    this.p_hand = []; // Gonna be an array
+    this.p_color = null;
+    this.p_victory_pts = null;
+    this.p_dev_cards = null;
   }
   present() {
     return "My name is " + this.p_name;
   }
   show_hand(){
-    for (i = 0; i < p_hand.length; i++) {
-      print(p_hand[i]); // this is going to have t
+    var i;
+    for (i = 0; i < this.p_hand.length; i++) {
+      this.p_hand[i].present(); // this calls the method in the Card class
     }
   }
+  add_card(new_card){
+    this.p_hand.push(new_card);
+  }  
 }
 
 // Card is done
 class Card {
-  constructor(name) {
-    this.resource = NULL;
+  constructor(resource) {
+    this.resource = resource;
   }
   present() {
-    return "Resource:  " + this.resource;
+    console.log("Resource: " + this.resource);
   }
 }
 
@@ -37,20 +40,53 @@ class Board {
 }
 
 class Tile {
-  constructor(brand) {
-    this.carname = brand;
+  constructor(resource, number) {
+    this.resource = resource;
+    this.number = number;
   }
   present() {
-    return "I have a " + this.carname;
+    const format = `   _____
+  /     \\ 
+ /       \\ 
+(    ` + this.resource + `    )
+ \\   ` + this.number + `   /
+  \\_____/`;
+    return format;
   }
 }
 
 // objects do not need declaration as var (seen as a container for variables)
-my_player = new Player("Twiggy");
+my_player = new Player("WildKoala");
+//console.log(my_player.present());
 
+
+my_card = new Card("W");
+my_player.add_card(my_card);
+
+my_card2 = new Card("O");
+my_player.add_card(my_card2);
+
+my_card3 = new Card("C");
+my_player.add_card(my_card3);
+
+my_player.show_hand();
+
+//my_tile = new Tile("W", 5);
+//console.log(my_tile.present());
 // their methods can be accessed like this.
-my_player.present();
+//card.present();
+
 /*
+console.log(
+`   _____
+  /     \\ 
+ /       \\ 
+(    ` + "W" + `    )
+ \\       /
+  \\_____/`);
+/*
+
+
 function main(){
 	main_menu(num_players, points_to_win)
 	create_board(size) // Create the board itself, with tiles that have a letter for the resource and number for the probability
